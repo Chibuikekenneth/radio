57c3ee018a36bff3321b6a1d8c21871854fb1377
Anthony Alaribe <anthonyalaribe@gmail.com>
2016-05-20 21:35:52 +0100

    the radio worls, giving same stream to multiple connections. time to optimize

---
+++ main.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"io"
+	//	"io/ioutil"
+	"bufio"
+	_ "expvar"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	connpool = []net.Conn{}
+)
+
+func conchandler(w http.ResponseWriter, r *http.Request, c *chan []byte) {
+	log.Println("con")
+
+	w.Header().Set("Content-Type", "audio/mpeg")
+	for cc := range *c {
+		log.Println("read something")
+		nr := len(cc)
+		log.Println(nr)
+
+		nw, ew := w.Write(cc[0:nr])
+		if ew == nil && nw != nr {
+			ew = io.ErrShortWrite
+		}
+		var err error
+		if ew != nil {
+			err = ew
+		}
+		if err != nil {
+			log.Println(err)
+		}
+
+	}
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	//log.Println("dfgdfg handler")
+	parentFolder := "./music"
+	file, err := os.Open(parentFolder)
+
+	if err != nil {
+		log.Println(err)
+	}
+	//log.Println(file)
+	files, err := file.Readdir(-1)
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "audio/mpeg")
+
+	playlist := []string{}
+	for _, v := range files {
+		curr := parentFolder + "/" + v.Name()
+		playlist = append(playlist, curr)
+	}
+
+	//	i := 0
+	for i := 0; i < len(playlist); i++ {
+		f, err := os.Open(playlist[i])
+		log.Println(playlist[i])
+		if err != nil {
+			log.Println(err)
+		}
+
+		buf := make([]byte, 100000)
+		offset := int64(0)
+		for {
+			nr, err := f.ReadAt(buf, offset)
+			if err != nil {
+				log.Println(err)
+			}
+			if nr == 0 {
+				//return 0, err
+				log.Printf("read %d", nr)
+				break
+			}
+			nw, ew := w.Write(buf[0:nr])
+			if ew == nil && nw != nr {
+				ew = io.ErrShortWrite
+			}
+			if ew != nil {
+				err = ew
+			}
+
+			offset = int64(nr) + int64(offset)
+			log.Println(offset)
+		}
+	} //	return int64(nw), err
+}
+
+func concplayer(c *[]*chan []byte) {
+	//log.Println("dfgdfg handler")
+	parentFolder := "./music"
+	file, err := os.Open(parentFolder)
+
+	if err != nil {
+		log.Println(err)
+	}
+	//log.Println(file)
+	files, err := file.Readdir(-1)
+	if err != nil {
+		log.Println(err)
+	}
+
+	playlist := []string{}
+	for _, v := range files {
+		curr := parentFolder + "/" + v.Name()
+		playlist = append(playlist, curr)
+	}
+
+	//	i := 0
+	for i := 0; i < len(playlist); i++ {
+		f, err := os.Open(playlist[i])
+		log.Println(playlist[i])
+		if err != nil {
+			log.Println(err)
+		}
+
+		buf := make([]byte, 128000)
+		offset := int64(0)
+		for {
+			nr, err := f.ReadAt(buf, offset)
+			if err != nil {
+				log.Println(err)
+			}
+			if nr == 0 {
+				//return 0, err
+				log.Printf("read %d", nr)
+				break
+			}
+			log.Println(len(*c))
+
+			for i, v := range *c {
+				log.Println(i)
+				//log.Println(*c[i])
+
+				*v <- buf[0:nr]
+
+			}
+			//	c <- buf[0:nr]
+			/*
+				for _, c := range connpool {
+					writer := bufio.NewWriter(c)
+
+					//	writer.Header().Set("Content-Type", "audio/mpeg")
+
+					nw, ew := writer.Write(buf[0:nr])
+					if ew == nil && nw != nr {
+						ew = io.ErrShortWrite
+					}
+					if ew != nil {
+						err = ew
+					}
+
+				}
+			*/
+			offset = int64(nr) + int64(offset)
+			log.Println(offset)
+			time.Sleep(2 * time.Second)
+		}
+	} //	return int64(nw), err
+
+}
+
+func player() {
+	//log.Println("dfgdfg handler")
+	parentFolder := "./music"
+	file, err := os.Open(parentFolder)
+
+	if err != nil {
+		log.Println(err)
+	}
+	//log.Println(file)
+	files, err := file.Readdir(-1)
+	if err != nil {
+		log.Println(err)
+	}
+
+	playlist := []string{}
+	for _, v := range files {
+		curr := parentFolder + "/" + v.Name()
+		playlist = append(playlist, curr)
+	}
+
+	//	i := 0
+	for i := 0; i < len(playlist); i++ {
+		f, err := os.Open(playlist[i])
+		log.Println(playlist[i])
+		if err != nil {
+			log.Println(err)
+		}
+
+		buf := make([]byte, 128000)
+		offset := int64(0)
+		for {
+			nr, err := f.ReadAt(buf, offset)
+			if err != nil {
+				log.Println(err)
+			}
+			if nr == 0 {
+				//return 0, err
+				log.Printf("read %d", nr)
+				break
+			}
+
+			for _, c := range connpool {
+				writer := bufio.NewWriter(c)
+
+				//	writer.Header().Set("Content-Type", "audio/mpeg")
+
+				nw, ew := writer.Write(buf[0:nr])
+				if ew == nil && nw != nr {
+					ew = io.ErrShortWrite
+				}
+				if ew != nil {
+					err = ew
+				}
+
+			}
+
+			offset = int64(nr) + int64(offset)
+			log.Println(offset)
+			time.Sleep(2 * time.Second)
+		}
+	} //	return int64(nw), err
+
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("hello world")
+
+	x := http.NewServeMux()
+
+	//	chanpool := make([]chan []byte, 10)
+	var chanpool []*chan []byte
+	count := 0
+	mutex := &sync.Mutex{}
+
+	go concplayer(&chanpool)
+
+	x.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		channel := make(chan []byte)
+		chanpool = append(chanpool, &channel)
+
+		//chanpool[count] = make(chan []byte)
+
+		mutex.Lock()
+		count = count + 1
+		mutex.Unlock()
+		log.Println(len(chanpool))
+		log.Print(count)
+		conchandler(w, r, chanpool[count-1])
+
+	})
+
+	err := http.ListenAndServe(":8080", x)
+	if err != nil {
+		log.Println(err)
+	}
+
+	/*	mutex := &sync.Mutex{}
+
+		ln, err := net.Listen("tcp", "localhost:8080")
+
+		if err != nil {
+			log.Fatal(err)
+
+		}
+		go func() {
+
+			for {
+				conn, err := ln.Accept()
+				if err != nil {
+					log.Println(err)
+					continue
+
+				}
+				log.Println(conn)
+				mutex.Lock()
+				connpool = append(connpool, conn)
+				mutex.Unlock()
+				//go HandleIncoming(conn)
+
+			}
+
+		}()
+
+		go player()
+
+		for t := range time.Tick(5 * time.Minute) {
+			log.Println(t)
+		}
+	*/
+}
